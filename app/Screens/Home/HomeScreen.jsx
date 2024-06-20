73db56e7e6ecc73fcc257f0974b195df256ed302
Sai Sashreek D <sai@5thdt.com>
2024-06-20 17:15:08 +0530

    Home Screen Updated with Map and EV Data

--- app/Screens/Home/HomeScreen.jsx
+++ app/Screens/Home/HomeScreen.jsx
@@ -1,12 +1,69 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Header from './Header'
+import SearchBar from './SearchBar'
+import AppMapView from './AppMapView'
+import { SelectMarkerContext } from '../../Context/SelectMarkerContext'
+import { UserLocationContext } from '../../Context/UserLocationContext'
+import GlobalApi from "../../Utils/GlobalApi"
 
 export default function HomeScreen() {
+
+  const { location, setLocation } = useContext(UserLocationContext);
+  const [placeList,setPlaceList]=useState([]);
+  const [selectedMarker,setSelectedMarker]=useState(0);
+  useEffect(()=>{
+    setSelectedMarker(0)
+    location&&GetNearByPlace();
+  },[location])
+
+  /**
+   * Used to get Near by place using google place api
+   */
+  const GetNearByPlace = () => {
+    const data = {
+      "includedTypes": ["electric_vehicle_charging_station"],
+      "maxResultCount": 10,
+      "locationRestriction": {
+        "circle": {
+          "center": {
+            "latitude": location?.latitude,
+            "longitude": location?.longitude
+          },
+          "radius": 5000.0
+        }
+      }
+    }
+    GlobalApi.NewNearByPlace(data).then(resp => {
+      setPlaceList(resp.data?.places);
+    })
+  }
+
   return (
+    <SelectMarkerContext.Provider value={{selectedMarker,setSelectedMarker}}>
     <View>
-      <Text>HomeScreen</Text>
+      <View style={styles.headerContainer}>
+        <Header />
+        <SearchBar />        
+      </View>
+      <AppMapView />
     </View>
+    </SelectMarkerContext.Provider>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'absolute',
+    zIndex: 10,
+    padding: 10,
+    width: '100%',
+    paddingHorizontal: 20
+  },
+  placeListContainer:{
+    position:'absolute',
+    bottom:0,
+    zIndex:10,
+    width:'100%'
+  }
+})
