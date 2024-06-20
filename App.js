ba7a372e4ddfe10b703309b721291779c0757c79
Sai Sashreek D <sai@5thdt.com>
2024-06-20 17:10:05 +0530

    Added Location Permission and User Location Context

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import * as SecureStore from "expo-secure-store";
+import * as Location from "expo-location";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import TabNvaigation from "./app/Navigation/TabNvaigation";
 import GlobalApi from "./app/Utils/GlobalApi";
 
-import Login from "./app/Screens/Login/LoginScreen"
+import Login from "./app/Screens/Login/LoginScreen";
+import { UserLocationContext } from "./app/Context/UserLocationContext";
 
 const tokenCache = {
   async getToken(key) {
@@ -29,6 +31,40 @@ const tokenCache = {
 };
 
 export default function App() {
+  const [location, setLocation] = useState({
+    latitude: 35.23112,
+    longitude: -80.8383,
+  });
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        console.log("DENINED");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+      if (!location?.coords) {
+        setLocation({
+          latitude: "35.231120",
+          longitude: "-80.838300",
+        });
+      }
+      console.log("--m", location.coords);
+    };
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   const [fontsLoaded] = useFonts({
     Jost: require("./assets/fonts/Jost-Regular.ttf"),
     JostT: require("./assets/fonts/Jost-Thin.ttf"),
@@ -50,18 +86,23 @@ export default function App() {
   }
 
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={GlobalApi.CLERK_API_KEY}>
-      <View style={styles.container} onLayout={onLayoutRootView}>
-        <SignedIn>
-          <NavigationContainer>
-            <TabNvaigation />
-          </NavigationContainer>
-        </SignedIn>
-        <SignedOut>
-          <Login />
-        </SignedOut>
-        <StatusBar style="auto" />
-      </View>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={GlobalApi.CLERK_API_KEY}
+    >
+      <UserLocationContext.Provider value={{ location, setLocation }}>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+          <SignedIn>
+            <NavigationContainer>
+              <TabNvaigation />
+            </NavigationContainer>
+          </SignedIn>
+          <SignedOut>
+            <Login />
+          </SignedOut>
+          <StatusBar style="auto" />
+        </View>
+      </UserLocationContext.Provider>
     </ClerkProvider>
   );
 }
