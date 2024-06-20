71562cf26d4dcbc6897eec5a9f40447725b2f79b
Sai Sashreek D <sai@5thdt.com>
2024-06-20 12:10:20 +0530

    App Entry file Created

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff"
+    }
+})
