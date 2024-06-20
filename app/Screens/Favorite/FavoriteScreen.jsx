b52dcc2e117ae6915198efa93a1e7d27fa0bbde4
Sai Sashreek D <sai@5thdt.com>
2024-06-20 17:11:30 +0530

    Favorite Screen Updated

--- app/Screens/Favorite/FavoriteScreen.jsx
+++ app/Screens/Favorite/FavoriteScreen.jsx
@@ -1,12 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, ActivityIndicator, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import Colors from "../../Utils/Colors";
+import { getFirestore } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { useUser } from "@clerk/clerk-expo";
+import { app } from "../../Utils/FirebaseConfig";
+import PlaceItem from "../Home/PlaceItem";
 
 export default function FavoriteScreen() {
+  const db = getFirestore(app);
+  const { user } = useUser();
+  const [favList, setFavList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    user && getFav();
+  }, [user]);
+
+  /**
+   * Get All Fav List list of places from firebase
+   */
+  const getFav = async () => {
+    setLoading(true);
+    setFavList([]);
+    const q = query(
+      collection(db, "ev-fav-place"),
+      where("email", "==", user?.primaryEmailAddress?.emailAddress)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      setFavList((favList) => [...favList, doc.data()]);
+      setLoading(false);
+    });
+  };
+
   return (
     <View>
-      <Text>FavoriteScreen</Text>
-    </View>
-  )
-}
+      <Text
+        style={{
+          padding: 10,
+          fontFamily: "JostM",
+          fontSize: 30,
+        }}
+      >
+        My Favorite
+        <Text style={{ color: Colors.PRIMARY }}> Place</Text>
+      </Text>
+      {!favList ? (
+        <View
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <ActivityIndicator size={"large"} color={Colors.PRIMARY} />
+          <Text style={{ fontFamily: "Jost", marginTop: 5 }}>Loading...</Text>
+        </View>
+      ) : null}
 
-const styles = StyleSheet.create({})
+      <FlatList
+        data={favList}
+        onRefresh={() => getFav()}
+        refreshing={loading}
+        style={{ paddingBottom: 200 }}
+        renderItem={({ item, index }) => (
+          <PlaceItem
+            place={item.place}
+            isFav={true}
+            markedFav={() => getFav()}
+          />
+        )}
+      />
+      <View style={{ marginBottom: 200, height: 200 }}></View>
+    </View>
+  );
+}
